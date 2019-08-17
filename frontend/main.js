d0b80d6a9ecffff1913be792bec001a157308278
m7Aei <mateuspm2015@hotmail.com>
2019-08-17 14:43:42 -0300

    Criando o package json e os arquivos principais

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+// dar npm install --save-dev electron
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Cria uma janela de navegação.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // e carregar o index.html do aplicativo.
+  win.loadFile("index.html");
+}
+
+app.on("ready", createWindow);

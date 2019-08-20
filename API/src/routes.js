d7b2b3a511593ec4dd0ab8a3059fdd469b9adaa5
m7Aei <mateuspm2015@hotmail.com>
2019-08-20 17:17:22 -0300

    Alterando algumas rotas

--- API/src/routes.js
+++ API/src/routes.js
@@ -2,15 +2,22 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.post('/signin', SessionController.store)
-
 routes.get('/user', UserController.index)
+routes.post('/user', guestMiddleware, UserController.store)
 routes.get('/user/:id', UserController.show)
-routes.post('/user', UserController.store)
-routes.put('/user/:id', UserController.update)
-routes.delete('/user/:id', UserController.destroy)
+
+routes.post('/signin', guestMiddleware, SessionController.store)
+
+routes.use('/app', authMiddleware)
+
+routes.get('app/logout', SessionController.destroy)
+routes.put('app/user/:id', UserController.update)
+routes.delete('app/user/:id', UserController.destroy)
 
 module.exports = routes

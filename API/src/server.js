938eddb1cc069083db0918c7a9706405210b5bfc
m7Aei <mateuspm2015@hotmail.com>
2019-08-20 17:17:55 -0300

    Configurando coisas da session no server.js

--- API/src/server.js
+++ API/src/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const session = require('express-session')
+const LokiStore = require('connect-loki')(session)
+const path = require('path')
 
 class App {
   constructor () {
@@ -11,6 +14,17 @@ class App {
 
   middlewares () {
     this.express.use(express.json())
+    this.express.use(
+      session({
+        name: 'root',
+        secret: 'APS',
+        resave: false,
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        saveUninitialized: true
+      })
+    )
   }
 
   routes () {

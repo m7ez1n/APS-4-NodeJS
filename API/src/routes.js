d60a1a7770c7bbce1f2041445475eab085fd76a1
m7Aei <mateuspm2015@hotmail.com>
2019-08-20 10:29:23 -0300

    Update nas rotas

--- API/src/routes.js
+++ API/src/routes.js
@@ -2,4 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/user', UserController.index)
+routes.get('/user/:id', UserController.show)
+routes.post('/user', UserController.store)
+routes.put('/user/:id', UserController.update)
+routes.delete('/user/:id', UserController.destroy)
+
 module.exports = routes

0c24c06e981e901f2eafad89e699a5fb50280130
m7Aei <mateuspm2015@hotmail.com>
2019-08-20 16:50:42 -0300

    Criando o metodo de checagem de senha

--- API/src/app/models/User.js
+++ API/src/app/models/User.js
@@ -20,5 +20,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   )
 
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
   return User
 }

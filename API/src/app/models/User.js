3226f04276ef8d0a0e55beef9c0fd6fb1390e83e
m7Aei <mateuspm2015@hotmail.com>
2019-08-20 16:43:02 -0300

    Fazendo a criptografia de senha

--- API/src/app/models/User.js
+++ API/src/app/models/User.js
@@ -1,9 +1,24 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password_hash: DataTypes.STRING
-  })
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  )
 
   return User
 }

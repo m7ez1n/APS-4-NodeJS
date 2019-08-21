27163f42d3d8c2515c40b15284ad62e75527142d
m7Aei <mateuspm2015@hotmail.com>
2019-08-21 10:23:42 -0300

    Criando a migration das Imagens

---
+++ API/src/database/migrations/20190821131833-create-images.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      images: {
+        allowNull: false,
+        type: Sequelize.BLOB
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users')
+  }
+}

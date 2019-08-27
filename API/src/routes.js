92cc33c799b3209ed66bfde7d1c915650da389ea
m7Aei <mateuspm2015@hotmail.com>
2019-08-27 10:02:18 -0300

    Atualizando as rotas

--- API/src/routes.js
+++ API/src/routes.js
@@ -4,23 +4,18 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-const authMiddleware = require('./app/middlewares/auth')
-const guestMiddleware = require('./app/middlewares/guest')
-
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
+// CRUD User
 routes.get('/user', UserController.index)
-routes.post('/user', guestMiddleware, UserController.store)
 routes.get('/user/:id', UserController.show)
+routes.post('/user', UserController.store)
+routes.put('/user/:id', UserController.update)
+routes.delete('/user/:id', UserController.destroy)
 
-routes.post('/signin', guestMiddleware, SessionController.store)
-
-routes.use('/app', authMiddleware)
-
-routes.get('app/logout', SessionController.destroy)
-routes.put('app/user/:id', UserController.update)
-routes.delete('app/user/:id', UserController.destroy)
+// Session
+routes.post('/sessions', SessionController.store)
 
 routes.post('app/images', upload.single('image'))
 

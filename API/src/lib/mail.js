a75749f3238cb4ee56b9b7186ec09693fd2d6753
m7Aei <mateuspm2015@hotmail.com>
2019-09-02 10:23:38 -0300

    Configurando o envio de email com templates

--- API/src/lib/mail.js
+++ API/src/lib/mail.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer')
+const { resolve } = require('path')
+const exphbs = require('express-handlebars')
+const nodemailerhbs = require('nodemailer-express-handlebars')
 const mailConfig = require('../config/mail')
 
 class Mail {
@@ -11,6 +14,26 @@ class Mail {
       secure,
       auth: auth.user ? auth : null
     })
+
+    this.configureTemplates()
+  }
+
+  configureTemplates () {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails')
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs'
+        }),
+        viewPath,
+        extName: '.hbs'
+      })
+    )
   }
 
   sendMail (message) {

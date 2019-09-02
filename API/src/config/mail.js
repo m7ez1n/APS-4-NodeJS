6f497735cc12296db1bd9d3fe92f6df691542009
m7Aei <mateuspm2015@hotmail.com>
2019-09-01 22:27:18 -0300

    Fazendo as configuraçoes padroes

---
+++ API/src/config/mail.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8bf2f8989e514d',
+    pass: 'a13a242e54cf02'
+  },
+  default: {
+    from: 'Equipe de desenvolvedores <noreply@aps.com>'
+  }
+}
